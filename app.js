16fe1b5346a7ea26778cf30ec000b5a250414f98
katos4 <gregoharriero@gmail.com>
2020-11-26 10:16:15 +0100

    cors 2

--- app.js
+++ app.js
@@ -26,15 +26,15 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json()); 
 
 /**cors*/
-//app.use(cors());
-app.use((req, res, next) => {
+app.use(cors());
+/*app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
  
     next();
-});
+});*/
 
 
 /** Usar las rutas cargadas previamente*/
